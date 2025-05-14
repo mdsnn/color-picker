d1929797979df81b6fb441b9db174db8844e318a
mwiko <mwikosinyangwe@gmail.com>
2025-05-14 10:39:30 +0200

    added some stuff

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -4,6 +4,10 @@ import './ColorPicker.css';
 function ColorPicker() {
   const [color, setColor] = useState('#ff0000');
 
+  const handleReset = () => {
+    setColor('#ff0000');
+  };
+
   return (
     <div>
       <h1>Color Picker</h1>
@@ -12,6 +16,7 @@ function ColorPicker() {
         value={color}
         onChange={(e) => setColor(e.target.value)}
       />
+      <button onClick={handleReset}>Reset</button>
       <div className="preview" style={{ backgroundColor: color }}></div>
       <p>Selected Color: {color}</p>
     </div>
